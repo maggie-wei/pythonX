f891fdfa1b37105e3694c069bec936500c6bbf9a
Maggie Wei <maggie.wei.cs@gmail.com>
2021-09-01 06:27:27 -0700

    v1.0

---
+++ Guess3.py
@@ -0,0 +1,11 @@
+# GuessANumber.py v1.0
+import random
+
+generatedNumber = random.randrange(1, 10)
+userGuess = int(input('Guess a number in the range 1-10:'))
+if userGuess == generatedNumber:
+    print("You are correct")
+elif userGuess < generatedNumber:
+    print("Your guess is too low")
+else:
+    print("Your guess is too high")
