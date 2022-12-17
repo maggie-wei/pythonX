505f4dfbb209260f1d08ab8ab664b106f897cab2
maggie-wei <70921594+maggie-wei@users.noreply.github.com>
2022-12-17 14:22:32 -0800

    Update Guess3.py

--- Guess3.py
+++ Guess3.py
@@ -9,7 +9,7 @@ userGuess = int(input('Guess a number in the range 1-10:'))
 if userGuess == generatedNumber:
     print("You are correct")
 elif userGuess < generatedNumber:
-    print("Your guess is too low!")
+    print("Your guess is too low !")
 else:
     print("Your guess is too high!")
          
