6adff84bce95f2b901f34d3e598d5e67c300e5ef
maggie-wei <70921594+maggie-wei@users.noreply.github.com>
2023-03-15 06:55:22 -0700

    Update Guess4.py

--- Guess4.py
+++ Guess4.py
@@ -5,6 +5,7 @@ generatedNumber = random.randrange(1, 10)
 userGuess = int(input('Guess a number in the range 1-10:'))
 if userGuess == generatedNumber:
     print("You are correct")
+    
 elif userGuess < generatedNumber:
     print("Your guess is too LOW")
     
